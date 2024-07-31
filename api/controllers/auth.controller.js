198dde3e8c82202dcb325d9104c8283491836822
tharinm <tharindumadhushan864@gmail.com>
2024-08-01 02:10:21 +0530

    cookie wip

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+
 import prisma from "../lib/prima.js";
 
 export const register = async (req, res) => {
@@ -20,13 +21,37 @@ export const register = async (req, res) => {
     });
 
     console.log(newUser);
-    res.status(201).json({ message: "User create successfully" })
-  }
-  catch (error){
-    res.status(500).json({message:"Failed to create user"})
+    res.status(201).json({ message: "User create successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to create user" });
   }
 };
 
-export const login = (req, res) => {};
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    // CHECK IF THE USER EXISTS
+    if (!user) return res.status(400).json({ message: "Invalid Credentials!" });
+
+    // CHECK IF THE PASSWORD IS CORRECT
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid)
+      return res.status(400).json({ message: "Invalid Credentials!" });
+
+     res.setHeader("Set-Cookie", "test=" + "myValue").json("success")
+    
+    // GENERATE COOKIE TOKEN AND SEND TO THE USER
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to login" });
+  }
+};
 
 export const logout = (req, res) => {};
