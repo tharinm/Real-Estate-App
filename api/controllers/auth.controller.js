e7db791f9f05d6ed5fefbf57eb2559ae90054d33
tharinm <tharindumadhushan864@gmail.com>
2024-08-01 01:17:46 +0530

    prisma model push for validtye

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,21 +4,27 @@ import prisma from "../lib/prima.js";
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
-  //HASH PWD
-  const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    //HASH PWD
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  console.log(hashedPassword);
+    console.log(hashedPassword);
 
-  // CREATE A NEW USER AND SAVE TO DB
-  const newUser = await prisma.user.create({
-    data: {
-      username,
-      email,
-      password: hashedPassword,
-    },
-  });
+    // CREATE A NEW USER AND SAVE TO DB
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
 
-  console.log(newUser);
+    console.log(newUser);
+    res.status(201).json({ message: "User create successfully" })
+  }
+  catch (error){
+    res.status(500).json({message:"Failed to create user"})
+  }
 };
 
 export const login = (req, res) => {};
