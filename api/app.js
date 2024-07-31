45586c33ed0eed168d70ae85edbdb536edcd9064
tharinm <tharindumadhushan864@gmail.com>
2024-08-01 00:27:17 +0530

    added prisma

--- api/app.js
+++ api/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import postRoutes from "./routes/post.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 
 app.listen(8800, () => {
---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+import bcrypt from "bcrypt";
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  //HASH PWD
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  console.log(hashedPassword);
+};
+
+export const login = (req, res) => {};
+
+export const logout = (req, res) => {};
