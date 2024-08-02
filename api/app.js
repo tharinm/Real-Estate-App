eece026a1c258d2e0c94be15ef2829884a379bac
tharinm <tharindumadhushan864@gmail.com>
2024-08-03 00:15:05 +0530

    cors added

--- api/app.js
+++ api/app.js
@@ -2,9 +2,17 @@ import express from "express";
 import postRoutes from "./routes/post.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
+const corsOptions = {
+  origin: 'http://localhost:5173', // Allow only this origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow credentials
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
--- client/src/routes/register/register.jsx
+++ client/src/routes/register/register.jsx
@@ -1,19 +1,52 @@
+import { useState } from "react";
 import "./register.scss";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Register() {
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    // Simulate API call
+    try {
+      const response = await axios.post("http://localhost:8800/api/auth/register", {
+        username,
+        email,
+        password,
+      });
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
   return (
     <div className="register">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
           <input name="password" type="password" placeholder="Password" />
-          <button >Register</button>
+          <button>Register</button>
+          {error && (
+        <div>
+          <p style={{ color: "red", fontSize: "10px" }}>
+            An error occurred while registering. Please try again.
+          </p>
+        </div>
+      )}
           <Link to="/login">Do you have an account?</Link>
         </form>
       </div>
+    
       <div className="imgContainer">
         <img src="/bg.png" alt="" />
       </div>
