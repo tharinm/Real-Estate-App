ecec2c97698bb307e79edd4bff85655d1e956e4c
tharinm <tharindumadhushan864@gmail.com>
2024-08-03 16:38:55 +0530

    middleware token add

--- api/controllers/test.controller.js
+++ api/controllers/test.controller.js
@@ -1,16 +1,8 @@
 import jwt from "jsonwebtoken";
 
 export const shouldBeLogin = async (req, res) => {
-  const token = req.cookies.token;
-
-  if (!token)
-    return res.status(401).json({ message: "User not authenticated!" });
-
-  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-    if (err) return res.status(401).json({ message: "User jwt invalid!" });
-  });
-
-  res.status(200).json({ message: "user Authenicated done !" });
+  console.log(req.userId);
+  res.status(200).json({ message: "You ar authenticated" });
 };
 
 export const shouldBeAdmin = async (req, res) => {
@@ -20,11 +12,11 @@ export const shouldBeAdmin = async (req, res) => {
     return res.status(401).json({ message: "User not authenticated!" });
 
   jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-      if (err) return res.status(401).json({ message: "User jwt invalid!" });
-      
-      if (!payload.isAdmin) {
-        return res.status(403).json({ message: "Not Authorized" });
-      }
+    if (err) return res.status(401).json({ message: "User jwt invalid!" });
+
+    if (!payload.isAdmin) {
+      return res.status(403).json({ message: "Not Authorized" });
+    }
   });
 
   res.status(200).json({ message: "user Authenicated done !" });
--- api/routes/test.route.js
+++ api/routes/test.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { shouldBeAdmin, shouldBeLogin } from "../controllers/test.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/should-be-log", shouldBeLogin);
+router.get("/should-be-log",verifyToken, shouldBeLogin);
 
 router.get("/should-be-admin", shouldBeAdmin);
 
