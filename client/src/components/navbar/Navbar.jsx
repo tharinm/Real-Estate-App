3bf41cf92c9b0fe5fc1271b4020f08d47d517d09
tharinm <tharindumadhushan864@gmail.com>
2024-08-03 23:44:41 +0530

    nav backend connect

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,11 +1,15 @@
 import "./navbar.scss";
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
 import brandLogo from "../../assets/homeestate.png";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { MdOutlineClose } from "react-icons/md";
-import { useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
+
+  const { currentUser } = useContext(AuthContext);
 
   return (
     <nav>
@@ -27,44 +31,37 @@ function Navbar() {
         </a>
       </div>
       <div className="right">
-        <a href="/" className="sign">
-          Sign In
-        </a>
-        <a href="/" className="register sign">
-          Sign Up
-        </a>
-        <div className="menu-icon">
-          {open ? (
-            <MdOutlineClose
-              color={open ? "white" : "black"}
-              onClick={() => setOpen((prev) => !prev)}
-            />
-          ) : (
-            <RxHamburgerMenu
-              color={open ? "white" : "black"}
-              onClick={() => setOpen((prev) => !prev)}
-            />
-          )}
+        {currentUser ? (
+          <div className="user">
+            <img src={currentUser.avatar || "/noavatar.jpg"} alt="" />
+            <span>{currentUser.username}</span>
+            <Link to="/profile" className="profile">
+              {/* {number > 0 && <div className="notification">{number}</div>} */}
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <a href="/login">Sign in</a>
+            <a href="/register" className="register">
+              Sign up
+            </a>
+          </>
+        )}
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
         </div>
-        <div className={open ? "menu active" : "menu disable-menu"}>
-          <a href="/" className="nav_link">
-            Home
-          </a>
-          <a href="/" className="nav_link">
-            About
-          </a>
-          <a href="/" className="nav_link">
-            Contact
-          </a>
-          <a href="/" className="nav_link">
-            Agents
-          </a>
-          <a href="/" className="nav_link">
-            Sign In
-          </a>
-          <a href="/" className="nav_link">
-            Sign Up
-          </a>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
         </div>
       </div>
     </nav>
