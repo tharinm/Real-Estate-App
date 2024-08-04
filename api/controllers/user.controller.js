0743ff06e40c77d44bc33bfd782baba72b6275ce
tharinm <tharindumadhushan864@gmail.com>
2024-08-04 12:52:05 +0530

    update add

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,6 @@
 import prisma from "../lib/prima.js";
+import bcrypt from "bcrypt";
+
 
 export const getUsers = async (req, res, next) => {
   try {
@@ -11,15 +13,47 @@ export const getUsers = async (req, res, next) => {
 };
 
 export const getUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id },
+    });
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get user!" });
+  }
+};
+
+export const updateUser = async (req, res) => {
     const id = req.params.id;
+    const tokenUserId = req.userId;
+    const { password, avatar, ...inputs } = req.body;
+  
+    if (id !== tokenUserId) {
+      return res.status(403).json({ message: "Not Authorized!" });
+    }
+  
+    let updatedPassword = null;
     try {
-      const user = await prisma.user.findUnique({
+      if (password) {
+        updatedPassword = await bcrypt.hash(password, 10);
+      }
+  
+      const updatedUser = await prisma.user.update({
         where: { id },
+        data: {
+          ...inputs,
+          ...(updatedPassword && { password: updatedPassword }),
+          ...(avatar && { avatar }),
+        },
       });
-      res.status(200).json(user);
+  
+      const { password: userPassword, ...rest } = updatedUser;
+  
+      res.status(200).json(rest);
     } catch (err) {
       console.log(err);
-      res.status(500).json({ message: "Failed to get user!" });
+      res.status(500).json({ message: "Failed to update users!" });
     }
   };
-  
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,17 +1,16 @@
 import express from "express";
 import {
-
   getUser,
   getUsers,
-  
+  updateUser,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
-router.get("/search/:id", verifyToken, getUser);
-// router.put("/:id", verifyToken, updateUser);
+router.get("/:id", verifyToken, getUser);
+router.put("/:id", verifyToken, updateUser);
 // router.delete("/:id", verifyToken, deleteUser);
 // router.post("/save", verifyToken, savePost);
 // router.get("/profilePosts", verifyToken, profilePosts);
