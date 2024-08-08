1e20746994491ebf54d316cda3ceb6b1042e7d14
tharinm <tharindumadhushan864@gmail.com>
2024-08-08 23:17:56 +0530

    delete user add

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -57,3 +57,23 @@ export const updateUser = async (req, res) => {
       res.status(500).json({ message: "Failed to update users!" });
     }
   };
+
+  export const deleteUser = async (req, res) => {
+    const id = req.params.id;
+    const tokenUserId = req.userId;
+  
+    if (id !== tokenUserId) {
+      return res.status(403).json({ message: "Not Authorized!" });
+    }
+  
+    try {
+      await prisma.user.delete({
+        where: { id },
+      });
+      res.status(200).json({ message: "User deleted" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Failed to delete users!" });
+    }
+  };
+  
