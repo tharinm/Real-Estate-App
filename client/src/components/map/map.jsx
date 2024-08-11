74702dc56996d9bcc1f5a1d97c4cef9e42c59f81
tharinm <tharindumadhushan864@gmail.com>
2024-08-11 11:21:22 +0530

    map fixed

--- client/src/components/map/map.jsx
+++ client/src/components/map/map.jsx
@@ -4,10 +4,13 @@ import "leaflet/dist/leaflet.css";
 import Pin from "../pin/pin";
 
 function Map({ items }) {
- 
   return (
     <MapContainer
-      center={[52.4797, -1.90269]}
+      center={
+        items.length === 1
+          ? [items[0].latitude, items[0].longitude]
+          : [52.4797, -1.90269]
+      }
       zoom={7}
       scrollWheelZoom={false}
       className="map"
